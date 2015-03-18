9e3e295f7d5aa099f8a5baa6e54fe10f30c7277f
kellygwiseman <kellygwiseman@users.noreply.github.com>
2015-03-17 17:16:21 -0700

    added setup script

---
+++ code/setup.py
@@ -0,0 +1,11 @@
+"""Create figure directory structure."""
+
+import os
+os.mkdir(../figures)
+os.mkdir(../figures/pushup_reps)
+os.mkdir(../figures/bp_filter)
+os.mkdir(../figures/correlation)
+os.mkdir(../figures/acceleration)
+os.mkdir(../figures/motion)
+os.mkdir(../figures/quaternion)
+os.mkdir(../figures/gyro)
